2230915f2d95d77994479b72cb3e048a7863871e
Danil <daniilgerasimenok@gamil.com>
2021-10-26 15:43:49 +0300

    gallery js

---
+++ museum/js/gallery.js
@@ -0,0 +1,40 @@
+// let pictureInnerContainer = document.querySelector(".picture-inner-container");
+// let img = document.createElement("img");
+// img.classList.add("gallery-img");
+// img.src = `assets/img/gallery-git/galery1.jpg`;
+// img.alt = `gallery1`;
+// pictureInnerContainer.append(img);
+
+let arrayGalleryPhoto = [
+    "assets/img/gallery-git/galery1.jpg",
+    "assets/img/gallery-git/galery2.jpg", 
+    "assets/img/gallery-git/galery3.jpg", 
+    "assets/img/gallery-git/galery4.jpg", 
+    "assets/img/gallery-git/galery5.jpg", 
+    "assets/img/gallery-git/galery6.jpg", 
+    "assets/img/gallery-git/galery7.jpg", 
+    "assets/img/gallery-git/galery8.jpg", 
+    "assets/img/gallery-git/galery9.jpg", 
+    "assets/img/gallery-git/galery10.jpg", 
+    "assets/img/gallery-git/galery11.jpg", 
+    "assets/img/gallery-git/galery12.jpg", 
+    "assets/img/gallery-git/galery13.jpg",
+    "assets/img/gallery-git/galery14.jpg",
+    "assets/img/gallery-git/galery15.jpg"
+]
+
+// function shuffle(img){
+//     return img.sort(() => Math.random() - 0.5);
+// }
+// shuffle(arrayGalleryPhoto);
+
+// function addElement(arr){
+//     let pictureInnerContainer = document.querySelector(".picture-inner-container");
+//     let img = document.createElement("img");
+//     img.classList.add("gallery-img");
+//     img.src = arr.sort(() => Math.random() - 0.5);
+//     img.alt = `gallery1`;
+//     pictureInnerContainer.append(img);
+//     arr.map()
+// }
+// addElement(arrayGalleryPhoto);
